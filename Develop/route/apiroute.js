b0ee1aadd9988184ec041e6be9d076ef755d4af6
Michael Jacob <michaelleojacob@gmail.com>
2020-04-10 23:30:45 -0700

    fixed a bug, had set the htmlroute to do *, and was NOT the last route to handle. So it was overriding other funtionallity of the code. started working on the delete function

--- Develop/route/apiroute.js
+++ Develop/route/apiroute.js
@@ -1,6 +1,7 @@
 const data = require("../db/db.json");
 const fs = require("fs");
 
+
 module.exports = function(app){
     app.get('/api/notes', function (req, res){
         //making comment for later, might be two dots.
@@ -16,18 +17,37 @@ module.exports = function(app){
         fs.readFile('./db/db.json', "utf-8", (err, notesReponse) => {
             let allNotes = JSON.parse(notesReponse);
             const newNote = req.body;
+            if(allNotes.length > 0){
+                newNote.id = allNotes[allNotes.length - 1].id + 1;
+            }else{
+                newNote.id = 1;
+            }
             allNotes = [...allNotes, newNote];
             console.log("allNotes", allNotes);
+            
             fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
                 if (err) throw res.status(500).json(err);
                 res.json(allNotes);
             } ) 
     })
 })
-}
 
 //delete
 //app.delete
-//path= api/note/:id
+//path= api/note/:id --- :id is a string, convert to number
 //loop through them, find id to delete
 //once i delete/filter, use fs.writefile write updated variable allnotes.
+
+app.delete('/api/notes/:id', function (req, res){
+    fs.readFile('./db/db.json', "utf-8", (err, notesReponse) => {
+        let allNotes = JSON.parse(notesReponse);
+        const noteIndex = allNotes.findIndex(allNotes => allNotes.id === req.params.id);
+    if (noteIndex === -1 ){
+        return res.sendStatus(404);
+    }
+    allNotes.splice(noteIndex, 1);
+    return res.sendStatus(200);
+    
+})
+});
+}
--- Develop/server.js
+++ Develop/server.js
@@ -8,8 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-require("./route/htmlroute")(app)
 require("./route/apiroute")(app)
+//this needs to be LAST
+require("./route/htmlroute")(app)
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
