538138522986d05b7d943bae73465897e6aab374
Michael Jacob <michaelleojacob@gmail.com>
2020-04-10 23:57:47 -0700

    added a small change to hopefully fix heroku deployment bug. made sure there was a / route

--- Develop/route/htmlroute.js
+++ Develop/route/htmlroute.js
@@ -3,6 +3,9 @@ const path = require("path");
 // Routing
 module.exports = function(app) {
   //HTML GET Routes
+    app.get("/", function(req, res) {
+      res.json(path.join(__dirname, "public/index.html"));
+    });
   app.get('/notes', function(req, res) {
       // "/notes" responds with the notes.html file
     res.sendFile(path.join(__dirname, '../public/notes.html'));
